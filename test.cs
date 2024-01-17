68015c4a359164473151591c7fb97171eff434b3
Maciej Bąba <maciek2baba2@gmail.com>
2024-01-17 21:01:41 +0100

    test idk

---
+++ test.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace YourNamespace.Tests
+{
+  [TestFixture]
+  public class TestClass
+  {
+    [Test]
+    public void TestMethod()
+    {
+      // Arrange
+
+      // Act
+      Console.WriteLine("Hello World!");
+      // Assert
+    }
+  }
+}
